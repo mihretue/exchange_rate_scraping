5fed76121f4b6c8f204b038fa26c02fd70e56659
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2024-08-31 09:42:56 +0300

    fetching the scraped data which is stored on db

--- api/models.py
+++ api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 class ScrapedData(models.Model):
-    data = models.JSONField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    currency_code = models.CharField(max_length=10)
+    currency_name = models.CharField(max_length=10)
+    cash_buying = models.DecimalField(max_digits=14, decimal_places=6)
+    cash_selling = models.DecimalField(max_digits=14, decimal_places=6)
+    
+    def __str__(self):
+        return self.currency_code
+    
 # Create your models here.
